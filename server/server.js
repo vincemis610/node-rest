cd9631c8dddd9ef091cb4d42e07466db1ad36c75
Vince Mis <vincemis610@gmail.com>
2020-05-27 11:45:28 -0500

    POST, PUT, DELETE Agregado

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// === GET HOME === //
+app.get('/', function (req, res) {
+  res.send({index: 'home'});
+});
+
 // === GET === //
 app.get('/usuario/:id', function (req, res) {
   let id = req.params.id;
@@ -30,13 +35,40 @@ app.post('/usuario', (req, res) => {
     })
   } else {
     res.json({
+      http: 'POST',
       persona: body
     });
   }
+});
 
+// === PUT === //
+app.put('/usuario', (req, res) => {
+  let body = req.body;
+
+  if (body.name === undefined) {
+    res.status(400).json({
+      response: false,
+      msg: 'El nombre es requerido'
+    })
+  } else {
+    res.json({
+      http: 'PUT',
+      persona: body
+    });
+  }
+});
+
+// === PUT === //
+app.delete('/usuario', (req, res) => {
+  
+    res.json({
+      http: 'DELETE',
+      msg: 'Deleted'
+    });
   
 });
  
+// === SETTING PORT === //
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto: ', process.env.PORT);
 })  
